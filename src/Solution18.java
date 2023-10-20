7d97bab5bf897ecbee47f8745da10bbd5b5b9d7c
MichaelAdebayo <mikea14071@gmail.com>
2023-10-20 15:23:12 -0400

    new

--- src/Solution18.java
+++ src/Solution18.java
@@ -1,26 +1,5 @@
 public class Solution18 {
-    public boolean halvesAreAlike(String s) {
-        char[] vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
-        int countVowels = 0;
-
-        String firstHalf = s.substring(0, s.length() / 2);
-        String secondHalf = s.substring(s.length() / 2);
-
-        for (int i = 0; i < vowels.length; i++) {
-            if (firstHalf.contains(String.valueOf(vowels[i]))) {
-                countVowels++;
-            }
-            if (secondHalf.contains(String.valueOf(vowels[i]))) {
-                countVowels--;
-            }
-        }
-
-        return countVowels == 0;
-    }
-}
-
-
-
+ 
     public boolean halvesAreAlike(String s) {
         // Convert the string to lowercase to handle uppercase and lowercase vowels
         s = s.toLowerCase();
